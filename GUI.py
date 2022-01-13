8c03fe360623de53b7c8f5ddf096ba08525a5a8a
RomiGe <65555952+Romi293@users.noreply.github.com>
2022-01-13 22:02:37 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,30 +3,41 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import messagebox
 
-import main2
+import main
 
 # True if REA is running, False if not
 running = False
+
+'''
+def changeBtnState():
+    if button.configure(image=startIcon):
+        print("OK")
+        button['image'] = pauseIcon
+        main.run_REA()
+    else:
+        button['image'] = startIcon
+'''
+
+
+def run():
+    print("Button clicked!")
+    main.run_REA()
+
 
 window = tk.Tk()
 window.configure(background='black')
 window.geometry('600x400')
 window.title('REA')
 
-
-def showMsg():
-    print('You clicked the Submit button!')
-    main2.run_REA()
-
-
 startIcon = tk.PhotoImage(file=r"pics/start_icon.png")  # with tk
+pauseIcon = tk.PhotoImage(file=r"pics/pause_icon.png")  # with tk
 logoIcon = ImageTk.PhotoImage(file=r"pics/logo.png")  # with PIL.ImageTk
 
 ReaImg = tk.Label(window, image=logoIcon, height=90, width=90)  # set image size
 ReaImg.place(x=10, y=10)  # set image position
 
-startBtn = tk.Button(window, image=startIcon, command=showMsg, height=25, width=25)  # set button size
-startBtn.place(x=280, y=350)  # set button position
+button = tk.Button(window, image=startIcon, command=run, height=25, width=25)  # set button size
+button.place(x=280, y=350)  # set button position
 # startBtn.pack()
 
 window.mainloop()
