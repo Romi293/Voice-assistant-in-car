f01a3ee891848979a6b10ae5a0f22dff4966bc26
RomiGe <65555952+Romi293@users.noreply.github.com>
2022-01-13 20:25:20 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,34 +1,32 @@
+# from tkinter import *
 import tkinter as tk
-from tk import *
-# pip install pillow
 from PIL import Image, ImageTk
+from tkinter import messagebox
 
 import main2
 
+# True if REA is running, False if not
+running = False
 
-def sayHello():
-    print("button clicked!")
-
-
-def openWindow():
-    window = tk.Tk()
-    window.configure(background='black')
-
-    # title = tk.Label(text="REA", foreground="yellow", width=50, height=50)
-
-    startIcon = tk.PhotoImage(file=r"pics/start.png")  # with tk
-    logoIcon = ImageTk.PhotoImage(file=r"pics/logo.png")  # with PIL.ImageTk
-
-    startBtn = tk.Button(window, image=startIcon, command=sayHello(), height=25, width=25)  # set button size
-    startBtn.place(x=280, y=300)  # set button position
-
-    ReaImg = tk.Label(window, image=logoIcon, height=90, width=90)  # set image size
-    ReaImg.place(x=10, y=10)  # set image position
-
-    window.title("REA")
-    window.geometry("600x400")
-    window.mainloop()
+window = tk.Tk()
+window.configure(background='black')
+window.geometry('600x400')
+window.title('REA')
 
 
-if __name__ == "__main__":
-    openWindow()
+def showMsg():
+    print('You clicked the Submit button!')
+    main2.run_REA()
+
+
+startIcon = tk.PhotoImage(file=r"pics/start_icon.png")  # with tk
+logoIcon = ImageTk.PhotoImage(file=r"pics/logo.png")  # with PIL.ImageTk
+
+ReaImg = tk.Label(window, image=logoIcon, height=90, width=90)  # set image size
+ReaImg.place(x=10, y=10)  # set image position
+
+startBtn = tk.Button(window, image=startIcon, command=showMsg, height=25, width=25)  # set button size
+startBtn.place(x=280, y=350)  # set button position
+# startBtn.pack()
+
+window.mainloop()
