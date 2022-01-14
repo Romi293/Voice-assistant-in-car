69796155b6b1a6ba0f9ed2209430e79bfba28876
RomiGe <65555952+Romi293@users.noreply.github.com>
2022-01-14 13:35:02 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -24,20 +24,30 @@ def run():
     main.run_REA()
 
 
-window = tk.Tk()
-window.configure(background='black')
-window.geometry('600x400')
-window.title('REA')
+def exit():
+    print("Exit button clicked")
+    main.exit_REA()
 
-startIcon = tk.PhotoImage(file=r"pics/start_icon.png")  # with tk
-pauseIcon = tk.PhotoImage(file=r"pics/pause_icon.png")  # with tk
-logoIcon = ImageTk.PhotoImage(file=r"pics/logo.png")  # with PIL.ImageTk
 
-ReaImg = tk.Label(window, image=logoIcon, height=90, width=90)  # set image size
-ReaImg.place(x=10, y=10)  # set image position
+def openWindow():
+    window = tk.Tk()
+    window.configure(background='black')
+    window.geometry('600x400')
+    window.title('REA')
 
-button = tk.Button(window, image=startIcon, command=run, height=25, width=25)  # set button size
-button.place(x=280, y=350)  # set button position
-# startBtn.pack()
+    startIcon = tk.PhotoImage(file=r"pics/start_icon.png")  # with tk
+    pauseIcon = tk.PhotoImage(file=r"pics/pause_icon.png")  # with tk
+    logoIcon = ImageTk.PhotoImage(file=r"pics/logo.png")  # with PIL.ImageTk
 
-window.mainloop()
+    ReaImg = tk.Label(window, image=logoIcon, height=90, width=90)  # set image size
+    ReaImg.place(x=10, y=10)  # set image position
+
+    button = tk.Button(window, image=startIcon, command=run, height=25, width=25)  # set button size
+    button.place(x=250, y=350)  # set button position
+    # startBtn.pack()
+
+    buttonExit = tk.Button(window, image=pauseIcon, command=exit, height=25, width=25)  # set button size
+    buttonExit.place(x=300, y=350)  # set button position
+    # startBtn.pack()
+
+    window.mainloop()
